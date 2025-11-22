c3b3e06a9f25c3b8f38ff07c771c055f7c184039
akamrzero <j1104@proton.me>
2025-11-22 13:49:13 +0100

    UI updates

--- src/widgets/seek_bar.py
+++ src/widgets/seek_bar.py
@@ -1,9 +1,15 @@
 import gi
+
+from ..utils.time_converter import ms_to_minutes
+
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, GObject
 from ..utils.event_bus import GEB
 
-class SeekBar(Gtk.Scale):
+
+
+
+class SeekBar2(Gtk.Scale):
     def __init__(self):
 
         self._seeking = False
@@ -54,6 +60,83 @@ class SeekBar(Gtk.Scale):
     def create_click_gesture(self):
         click_gesture = None
         controllers = self.observe_controllers()
+        for i in range(controllers.get_n_items()):
+            controller = controllers.get_item(i)
+            if isinstance(controller, Gtk.GestureClick):
+                click_gesture = controller
+                break
+
+        click_gesture.set_button(0)  # listen for any button
+
+        click_gesture.connect("pressed", self._on_pressed)
+        click_gesture.connect("released", self._on_released)
+
+class SeekBar(Gtk.Box):
+    def __init__(self):
+
+        self._seeking = False
+        self._adjustment = Gtk.Adjustment(
+            value=0,
+            lower=0,
+            upper=1,
+            step_increment=0,
+            page_increment=0,
+            page_size=0
+        )
+        super().__init__(orientation=Gtk.Orientation.VERTICAL)
+        self._seekbar = Gtk.Scale()
+        self._seekbar.set_adjustment(self._adjustment)
+        self._seekbar.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.append(self._seekbar)
+
+        self.time_1_label = Gtk.Label(halign=Gtk.Align.START)
+        self.time_2_label = Gtk.Label(halign=Gtk.Align.END, hexpand=True)
+
+        bottom_wrapper = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        bottom_wrapper.append(self.time_1_label)
+        bottom_wrapper.append(self.time_2_label)
+        self.append(bottom_wrapper)
+
+
+        self.create_click_gesture()
+
+        GEB.connect_playback_progress(self._on_playback_progress)
+
+
+
+    @property
+    def duration(self):
+        return self._adjustment.get_upper()
+
+    @duration.setter
+    def duration(self, value):
+        self._adjustment.set_upper(value)
+        self.time_2_label.set_label(ms_to_minutes(value))
+
+    @property
+    def value(self):
+        return self._adjustment.get_value()
+
+    @value.setter
+    def value(self, value):
+        if not self._seeking:
+            self._adjustment.set_value(value)
+            self.time_1_label.set_label(ms_to_minutes(value))
+
+    def _on_pressed(self, gesture, n_press, x, y):
+        self._seeking = True
+
+    def _on_released(self, gesture, n_press, x=0, y=0):
+        self._seeking = False
+        # self.emit('position-seeked', (self._adjustment.get_value()))
+        GEB.emit_playback_position_seeked(self._adjustment.get_value())
+
+    def _on_playback_progress(self, _, progress):
+        self.value = progress
+
+    def create_click_gesture(self):
+        click_gesture = None
+        controllers = self._seekbar.observe_controllers()
         for i in range(controllers.get_n_items()):
             controller = controllers.get_item(i)
             if isinstance(controller, Gtk.GestureClick):
